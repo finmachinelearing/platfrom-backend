2084e3709c53c04ab7da5a9f7e3b31e2eb632a0e
nikita <npetrashin@ptsecurity.com>
2023-11-07 23:19:35 +0300

    updated

--- app/app/config.py
+++ app/app/config.py
@@ -8,3 +8,8 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 DATABASE_URL = os.environ['DATABASE_URL']
 DATABASE_MAX_POOL = int(os.environ.get('DATABASE_MAX_POOL', 5))
 DATABASE_MAX_OVERFLOW = int(os.environ.get('DATABASE_MAX_OVERFLOW', 5))
+
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+MAIL_SSO_CLIENT_ID = os.environ['MAIL_SSO_CLIENT_ID']
+MAIL_SSO_CLIENT_SECRET = os.environ['MAIL_SSO_CLIENT_SECRET']
--- app/app/utils.py
+++ app/app/utils.py
@@ -1,5 +1,12 @@
 import json
+from typing import Dict, Any
+
+from fastapi_sso.sso.base import OpenID
 
 
 def dumps(d):
     return json.dumps(d, ensure_ascii=False)
+
+
+def convert_openid(response: Dict[str, Any]) -> OpenID:
+    return OpenID(display_name=response['sub'])
